8064012a0e46d9f646d93561f5937a0bc67ffa0a
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-07 15:52:21 +0900

    example-table inserted

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
   root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/techs" => "home#techs"
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
+  get "users/" => "users#index"
   
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
