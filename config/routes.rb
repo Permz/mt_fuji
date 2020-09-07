bc38258a1016746752be6cfdfb7d57be1fa2391f
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-07 12:27:40 +0900

    set login || signin navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root "home#top"
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/techs" => "home#techs"
   get "/qanda" => "home#qanda"
   get "/news" => "home#news"
-  
+  get 'sessions/new'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
