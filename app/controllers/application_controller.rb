320518307bf491b38170156da84d81cfc35eb681
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-08 10:23:10 +0900

    tiny adjustment 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from Exception, with: :error_500 unless Rails.env.development?
+
+  rescue_from AbstractController::ActionNotFound, with: :error_404 unless Rails.env.development?
+  rescue_from ActionController::RoutingError, with: :error_404 unless Rails.env.development?
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404 unless Rails.env.development?
+
+  def error_500
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
+
+  def error_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+  
   helper_method :current_user
   # before_action :login_required#全てのアクションを実行する前に、login_requiredメソッドを実行する
 
